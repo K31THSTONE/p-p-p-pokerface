0034dabb8a16677950e7af750cf6315875dc8be1
goliff@ycp.edu <goliff@ycp.edu@kec119-19>
2017-11-02 11:44:33 -0400

    Finished

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,65 +1,77 @@
 (ns p-p-p-pokerface)
+ 
 
-(defn rank [card]
-  (let [[f _] card]
-    (if (Character/isDigit f)
-      (Integer/valueOf (str f))
-      (get face-value-map f))))
+ (defn rank [card]
+  (def replacements {\T 10,
+                     \J 11,
+                     \Q 12,
+                     \K 13,
+                     \A 14})
+  (let [[rank _] card]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (replacements rank))))
+ 
+ 
 
-(defn suit [card]
-  (let [[s _] card]
-    (str s)))
+ (defn suit [card]
+  (let [[_ suit] card]
+    (str suit)))
+ 
 
-(defn has-count-of-any-rank? [hand c]
-  (let [ranks (map rank hand)
-        freqs (vals (frequencies ranks))]
-    (< 0 (count (filter (fn [x] (= x c)) freqs)))))
+ (defn pair? [hand]
+  (= 2 (apply max (vals (frequencies (map rank hand))))))
+ 
+ 
 
-(defn pair? [hand]
-  (has-count-of-any-rank? hand 2))
+ (defn three-of-a-kind? [hand]
+  (= 3 (apply max (vals (frequencies (map rank hand))))))
+ 
+ 
 
-(defn three-of-a-kind? [hand]
-  (has-count-of-any-rank? hand 3))
+ (defn four-of-a-kind? [hand]
+  (= 4 (apply max (vals (frequencies (map rank hand))))))
+ 
+ 
 
-(defn four-of-a-kind? [hand]
-  (has-count-of-any-rank? hand 4))
+ (defn flush? [hand]
+  (= 5 (apply max (vals (frequencies (map suit hand))))))
+ 
+ 
 
-(defn flush? [hand]
-  (let [suits (map suit hand)
-        suit-freqs (vals (frequencies suits))
-        ]
-    (= 5 (apply min suit-freqs))))
+ (defn full-house? [hand]
+  (= [2 3] (sort (vals (frequencies (map rank hand))))))
+ 
+ 
 
-(defn full-house? [hand]
-  (let [rank-freq (sorted-rank-freqs hand)]
-    (= [2 3] rank-freq)))
+ (defn two-pairs? [hand]
+  (or (= [1 2 2] (sort (vals (frequencies (map rank hand)))))
+      (four-of-a-kind? hand)))
+ 
+ 
+ (defn straight? [hand]
+  (let [sorted-hand (sort (map rank hand))
+        minimum (apply min sorted-hand)]
+    (or (= (range minimum (+ minimum 5)) sorted-hand)
+        (= [2 3 4 5 14] sorted-hand))))
+ 
+ 
+ (defn straight-flush? [hand]
+  (and (straight? hand)
+       (flush? hand)))
 
-(defn two-pairs? [hand]
-  (let [rf (sorted-rank-freqs hand)]
-    (or
-      (= [1 2 2] rf)
-      (= [1 4] rf))))
-
-(defn straight? [hand]
-  (let [high-ace-rks (sort (map rank hand))
-        low-ace-rks (sort (replace {14 1} high-ace-rks))
-        straight-seq? (fn [seq]
-                        (= (range (apply min seq) (+ 5 (apply min seq))) seq))]
-    (or (straight-seq? high-ace-rks) (straight-seq? low-ace-rks))))
-
-(defn straight-flush? [hand]
-  (and (straight? hand (flush? hand))))
-
-(defn value [hand]
-  (let [checkers #{[high-card? 0] [pair? 1]
-                   [two-pairs? 2] [three-of-a-kind? 3]
-                   [straight? 4] [flush? 5]
-                   [full-house? 6] [four-of-a-kind? 7]
-                   [straight-flush? 8]}
-        check-and-get-value (fn [c]
-                              (if ((first c) hand)
-                                (second c)
-                                false))
-        all-values (map check-and-get-value checkers)
-        not-false-values (filter (fn [x] x) all-values)]
-    (apply max not-false-values)))
+(defn high-card? [hand]
+  true)
+ 
+ 
+ (defn value [hand]
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    (high-card? hand) 0))
